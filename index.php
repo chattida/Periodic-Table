a1921f3db8f0943a525fd770b4984d0056f2ae9a
rapeekhung <43008759+rapeeza1598@users.noreply.github.com>
2019-10-22 23:10:09 +0700

    edit lang :pencil: :pencil:
    edit en.html to en.php and th.html to th.php

--- index.php
+++ index.php
@@ -1,3 +1,31 @@
 <?php
+session_start();
+function page_count(){
+        if(isset($_SESSION['page_count'])){
+                $_SESSION['page_count'] += 1;
+                return $_SESSION['page_count'];
+        }else{
+                $_SESSION['page_count'] = 1;
+                return $_SESSION['page_count'];
+        }
+}
 
+function set_cookie(){
+        $count_loadpage = page_count();
+        $cookie_value = "th";
+        if($count_loadpage<1){
+                setcookie("lang", $cookie_value, time() + (86400 * 30), "/");
+        }else{
+                return $cookie_value;
+        }
+}
+function page_lang(){
+        if (!isset($_COOKIE[set_cookie()])) {
+        include("./lang/th.php");
+        }
+        else {
+        include("./lang/en.php");
+        }
+}
+page_lang();
 ?>
