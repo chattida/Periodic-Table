62acb77d78f40b64c243f5f965839923609cfb9c
Napat Arayawattanapong <41178313+NAPATKRUP@users.noreply.github.com>
2019-10-23 22:32:12 +0700

    ⭐️ Add Group Bar

--- js/myScript/table.js
+++ js/myScript/table.js
@@ -34,10 +34,10 @@ function showTable(obj) {
                 txt += `<td class="empty-box"></td>`
             } else if (i == 6 && j == 3) {
                 // row = 6, col = 3
-                txt += `<td class="element-box text-center cl_Lanthanide"><number>57-71</number></td>`
+                txt += `<td class="element-box text-center cl_Lanthanide" id="119"><number>57-71</number></td>`
             } else if (i == 7 && j == 3) {
                 // row = 7, col = 3
-                txt += `<td class="element-box text-center cl_Actinide"><number>89-103</number></td>`
+                txt += `<td class="element-box text-center cl_Actinide" id="120"><number>89-103</number></td>`
             } else if (i == 8) {
                 // row = 8
                 txt += `<td class="empty-box"></td>`
@@ -69,7 +69,7 @@ function showTable(obj) {
                 else if(`${obj[count].Type}` == "แลนทาไนด์" || `${obj[count].Type}` == "Lanthanide") txt += `cl_Lanthanide`;
                 else if(`${obj[count].Type}` == "แอกทิไนด์" || `${obj[count].Type}` == "Actinide") txt += `cl_Actinide`;
                 else if(`${obj[count].Type}` == "") txt += `cl_artificial`;
-                txt += `">
+                txt += `" id="${obj[count].AtomicNumber}" onmouseover="toshow(${obj[count].AtomicNumber})">
                             <box>
                                 <number>${obj[count].AtomicNumber}</number>
                                 <symbol>${obj[count].Symbol}</symbol>
@@ -83,6 +83,36 @@ function showTable(obj) {
         txt += `</tr>`
     }
     table.innerHTML = txt
+}
+
+//กลุ่มธาติ
+function mygroup(choose) {
+    for(let i=1; i<=120; i++) {
+        myset = document.getElementById(i);
+        if((choose == 1 && (i == 1 || i == 3 || i == 11 || i == 19 || i == 37 || i == 55 || i == 87)) ||
+           (choose == 2 && (i == 4 || i == 12 || i == 20 || i == 38 || i == 56 || i == 88)) ||
+           (choose == 3 && (i == 21 || i == 39 || (i >= 57 && i <= 71) || (i >= 89 && i <= 103) || i == 119 || i == 120)) ||
+           (choose == 4 && (i == 22 || i == 40 || i == 72 || i == 104)) ||
+           (choose == 5 && (i == 23 || i == 41 || i == 73 || i == 105)) ||
+           (choose == 6 && (i == 24 || i == 42 || i == 74 || i == 106)) ||
+           (choose == 7 && (i == 25 || i == 43 || i == 75 || i == 107)) ||
+           (choose == 8 && (i == 26 || i == 44 || i == 76 || i == 108)) ||
+           (choose == 9 && (i == 27 || i == 45 || i == 77 || i == 109)) ||
+           (choose == 10 && (i == 28 || i == 46 || i == 78 || i == 110)) ||
+           (choose == 11 && (i == 29 || i == 47 || i == 79 || i == 111)) ||
+           (choose == 12 && (i == 30 || i == 48 || i == 80 || i == 112)) ||
+           (choose == 13 && (i == 5 || i == 13 || i == 31 || i == 49 || i == 81 || i == 113)) ||
+           (choose == 14 && (i == 6 || i == 14 || i == 32 || i == 50 || i == 82 || i == 114)) ||
+           (choose == 15 && (i == 7 || i == 15 || i == 33 || i == 51 || i == 83 || i == 115)) ||
+           (choose == 16 && (i == 8 || i == 16 || i == 34 || i == 52 || i == 84 || i == 116)) ||
+           (choose == 17 && (i == 9 || i == 17 || i == 35 || i == 53 || i == 85 || i == 117)) ||
+           (choose == 18 && (i == 2 || i == 10 || i == 18 || i == 36 || i == 54 || i == 86 || i == 118)) ||
+           (choose == "x")) {
+            myset.style.opacity = "1";
+        }else {
+            myset.style.opacity = "0.25";
+        }
+    }
 }
 
 function getCookie(name) {
