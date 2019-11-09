58c553e80004bba2e8730e05c64247c95d856527
Chetsadaporn Traivinidsreesuk <james333james.ct@gmail.com>
2019-11-09 16:47:59 +0700

    ✏ Edit quiz.js

--- js/myScript/quiz.js
+++ js/myScript/quiz.js
@@ -33,7 +33,59 @@ function generateQuiz(obj) {
 
 function displayQuiz(obj) {
     json_obj = obj
-    console.log(json_obj[2][3].Element)
+    let score = 0, correct = [], states = ["ของแข็ง", "ของเหลว", "แก็ส", "ไม่ทราบ"]
+    let presetQ = [0, 1, 2, 3, 4]
+    for (i = 0; i < 5; i++) {
+        correct = (json_obj[i][Math.floor(Math.random() * json_obj[i].length)])
+        switch (presetQ.splice(Math.floor(Math.random() * presetQ.length), 1)[0]) {
+            case 0:
+                //ชื่อเต็มของธาตุน้คืออะไร
+                quizDialog = "ชื่อเต็มของธาตุ " + correct.Symbol + " คืออะไร"
+                console.log(quizDialog)
+                for (j in json_obj[i]){
+                    console.log(json_obj[i][j].Element)
+                }
+                console.log(correct)
+                break
+            case 1:
+                //ชื่อย่อของธาตุน้คืออะไร
+                quizDialog = "ชื่อย่อของธาตุ " + correct.Element + " คืออะไร"
+                console.log(quizDialog)
+                for (j in json_obj[i]){
+                    console.log(json_obj[i][j].Symbol)
+                }
+                console.log(correct)
+                break
+            case 2:
+                //ธาตุอยู่หมุ่อะไร
+                quizDialog = "ธาตุ " + correct.Element + " อยู่หมุ่อะไร"
+                console.log(quizDialog)
+                for (j in json_obj[i]){
+                    console.log(json_obj[i][j].Element)
+                }
+                console.log(correct)
+                break
+            case 3:
+                //เลขอะตอมธาตุน้คืออะไร
+                quizDialog = "เลขอะตอมของธาตุ " + correct.Element + " คืออะไร"
+                console.log(quizDialog)
+                for (j in json_obj[i]){
+                    console.log(json_obj[i][j].Element)
+                }
+                console.log(correct)
+                break
+            case 4:
+                //ธาตุนีทีสถานะอะไร
+                quizDialog = "ธาตุ " + correct.Element + " นีทีสถานะอะไร"
+                console.log(quizDialog)
+                for (j in states){
+                    console.log(states[j])
+                }
+                console.log(correct)
+                break
+        }
+    }
+    // console.log(correct)
 }
 
 // get cookie function
