a0c64e8bfa5bd1fe3ff90e04b5f01b3261a5161f
Chanwit Settavongsin <max180643@gmail.com>
2019-11-10 22:40:52 +0700

    :zap: Add checking empty cookie & wrong cookie value

--- data.php
+++ data.php
@@ -1,10 +1,25 @@
 <?php
-    if ($_COOKIE['lang'] == 'th') {
-        include("./php/data/th.php");
-    } else if ($_COOKIE['lang'] == 'en') {
-        include("./php/data/en.php");
-    } else {
-        include("./php/data/th.php");
-        setcookie("lang", "th", time() + (86400 * 30), "/");
+    function page_lang() {
+        if (check_cookie()) {
+            include("./php/data/th.php");
+        } else if ($_COOKIE['lang'] == "th") {
+            include("./php/data/th.php");
+        } else if ($_COOKIE['lang'] == "en") {
+            include("./php/data/en.php");
+        } else {
+            setcookie("lang", "th", time() + (86400 * 30), "/");
+            include("./php/data/th.php");
+        }
     }
+
+    function check_cookie() {
+        if (!isset($_COOKIE['lang'])) {
+            setcookie("lang", "th", time() + (86400 * 30), "/");
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    page_lang();
 ?>
--- list.php
+++ list.php
@@ -1,10 +1,25 @@
 <?php
-    if ($_COOKIE['lang'] == 'th') {
-        include("./page/list/th.html");
-    } else if ($_COOKIE['lang'] == 'en') {
-        include("./page/list/en.html");
-    } else {
-        include("./page/list/th.html");
-        setcookie("lang", "th", time() + (86400 * 30), "/");
+    function page_lang() {
+        if (check_cookie()) {
+            include("./page/list/th.html");
+        } else if ($_COOKIE['lang'] == "th") {
+            include("./page/list/th.html");
+        } else if ($_COOKIE['lang'] == "en") {
+            include("./page/list/en.html");
+        } else {
+            setcookie("lang", "th", time() + (86400 * 30), "/");
+            include("./page/list/th.html");
+        }
     }
+
+    function check_cookie() {
+        if (!isset($_COOKIE['lang'])) {
+            setcookie("lang", "th", time() + (86400 * 30), "/");
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    page_lang();
 ?>
